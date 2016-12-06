a2498e9a1ed1dad49ff2ca9119cdf98ecfbc557e
Aron <aron.c.thomas@gmail.com>
2016-12-06 11:49:02 -0600

    code cleanup

--- src/advent_of_code/exercises/day_1.clj
+++ src/advent_of_code/exercises/day_1.clj
@@ -4,8 +4,6 @@
 
 (def data (slurp (io/resource "day1.txt")))
 
-(def any? (complement not-any?))
-
 ;atom keeps track of direction you are facing and how far horizontally or vertically you have travelled
 (def navigation-atom
   (atom {:direction 0 :vertical-displacement 0 :horizontal-displacement 0}))
@@ -14,31 +12,14 @@
 
 (def cardinal-directions ["N" "E" "S" "W"])
 
-(defn move-user [direction distance]
+(defn move-user [direction]
   (cond
-    (= direction "N")     (swap! navigation-atom update-in [:vertical-displacement] + distance)
-    (= direction "S")     (swap! navigation-atom update-in [:vertical-displacement] - distance)
-    (= direction "E")     (swap! navigation-atom update-in [:horizontal-displacement] + distance)
-    (= direction "W")     (swap! navigation-atom update-in [:horizontal-displacement] - distance))
+    (= direction "N")     (swap! navigation-atom update-in [:vertical-displacement] inc)
+    (= direction "S")     (swap! navigation-atom update-in [:vertical-displacement] dec)
+    (= direction "E")     (swap! navigation-atom update-in [:horizontal-displacement] inc)
+    (= direction "W")     (swap! navigation-atom update-in [:horizontal-displacement] dec))
   (swap! location-tracker-atom conj (dissoc @navigation-atom :direction)))
 
-;only track location corresponding to movements
-(defn hop [x track-movement]
-  (let [orientation   (str (first x))
-        distance      (read-string (string/join (rest x)))]
-    (if (= orientation "R")
-      (swap! navigation-atom update-in [:direction] inc)
-      (swap! navigation-atom update-in [:direction] dec))
-    (let [direction     (nth
-                           cardinal-directions
-                           (mod (get @navigation-atom :direction) 4))]
-      (if
-        track-movement
-        (doseq [y (range 0 distance)]
-          (move-user direction 1))
-        (move-user direction distance)))))
-
-;track all points crossed
 (defn step [x]
   (let [orientation   (str (first x))
         distance      (read-string (string/join (rest x)))]
@@ -49,7 +30,8 @@
                            cardinal-directions
                            (mod (get @navigation-atom :direction) 4))]
       (doseq [y (range 0 distance)]
-        (move-user direction 1)))))
+        (move-user direction)))))
+
 
 (defn find-overlap [previously-visited other-nodes]
   (let [current-node   (first other-nodes)
@@ -65,24 +47,18 @@
       (recur (conj previously-visited current-node) visiting-next))))
 
 (defn part-1 []
-  (let [vector-path (string/split data #", ")]
-    (doseq [x vector-path]
-      (hop x false))
-    (println (+ (Math/abs (get @navigation-atom :horizontal-displacement))
-                (Math/abs (get @navigation-atom :vertical-displacement))))))
+  (println
+   "Answer to Part One:"
+   (+ (Math/abs (get @navigation-atom :horizontal-displacement))
+      (Math/abs (get @navigation-atom :vertical-displacement)))))
 
 (defn part-2 []
-  (let [vector-path (string/split data #", ")]
-    (doseq [x vector-path]
-      (hop x true))
-
-    (println
-     "Answer to Part One:"
-     (+ (Math/abs (get @navigation-atom :horizontal-displacement))
-        (Math/abs (get @navigation-atom :vertical-displacement))))
-
-    (find-overlap [(first @location-tracker-atom)] (rest @location-tracker-atom))))
-
+  (find-overlap [(first @location-tracker-atom)] (rest @location-tracker-atom)))
 
 (defn run []
-  (part-2))
+  (println "_________________DAY 1: NO TIME FOR A TAXICAB_________________")
+  (let [vector-path (string/split data #", ")]
+    (doseq [x vector-path]
+      (step x))
+    (part-1)
+    (part-2)))
