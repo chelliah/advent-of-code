4792ede2f662586be75efbfcce40f58a57e1a7c0
Aron <aron.c.thomas@gmail.com>
2016-12-06 20:50:21 -0600

    finish part 2

--- src/advent_of_code/exercises/day_2.clj
+++ src/advent_of_code/exercises/day_2.clj
@@ -6,8 +6,20 @@
 
 (def keyboard [[1 2 3] [4 5 6] [7 8 9]])
 
+(def keyboard-2 [[nil nil 1 nil nil] [nil 2 3 4 nil] [5 6 7 8 9] [nil "A" "B" "C" nil] [nil nil "D" nil nil]])
+
 ;finger position is tracked based on distance from the '5' key
-(def finger-position (atom {:horizontal-displacement 1 :vertical-displacement 1}))
+;part 1
+; (def finger-position (atom {:horizontal-displacement 1 :vertical-displacement 1}))
+
+;part 2
+(def finger-position (atom {:horizontal-displacement 0 :vertical-displacement 2}))
+
+(defn get-number-by-finger-position [keyboard vertical-displacement horizontal-displacement]
+  (nth
+   (nth keyboard vertical-displacement nil)
+   horizontal-displacement
+   nil))
 
 (defn move [movement]
   (case movement
@@ -15,6 +27,30 @@
     "D" (swap! finger-position update-in [:vertical-displacement] inc)
     "L" (swap! finger-position update-in [:horizontal-displacement] dec)
     "R" (swap! finger-position update-in [:horizontal-displacement] inc)))
+
+(defn can-move-2? [movement]
+  (case movement
+    "U" (not (nil?
+              (get-number-by-finger-position
+               keyboard-2
+               (dec (get @finger-position :vertical-displacement))
+               (get @finger-position :horizontal-displacement))))
+     "D" (not (nil?
+               (get-number-by-finger-position
+                keyboard-2
+                (inc (get @finger-position :vertical-displacement))
+                (get @finger-position :horizontal-displacement))))
+      "L" (not (nil?
+                (get-number-by-finger-position
+                 keyboard-2
+                 (get @finger-position :vertical-displacement)
+                 (dec (get @finger-position :horizontal-displacement)))))
+       "R" (not (nil?
+                 (get-number-by-finger-position
+                  keyboard-2
+                  (get @finger-position :vertical-displacement)
+                  (inc (get @finger-position :horizontal-displacement)))))))
+
 
 (defn can-move? [movement]
   (case movement
@@ -26,13 +62,14 @@
 (defn get-digit [sequence]
   (let [single-digit-sequence     (string/split sequence #"")]
     (doseq [movement single-digit-sequence]
-      (if (can-move? movement)
-        (move movement)))
-    ; (println (get @finger-position :vertical-displacement))))
-    (nth
-     (nth keyboard
-          (get @finger-position :vertical-displacement))
-     (get @finger-position :horizontal-displacement))))
+      ; (if (can-move? movement)
+      ;   (move movement)))
+      (if (can-move-2? movement)
+         (move movement)))
+    (get-number-by-finger-position
+      keyboard-2
+      (get @finger-position :vertical-displacement)
+      (get @finger-position :horizontal-displacement))))
 
 
 (defn run []
